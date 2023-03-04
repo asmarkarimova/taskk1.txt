386e9a46f3c3208be12145283419c51cf2b43921
asmar <126621069+asmarkarimova@users.noreply.github.com>
2023-03-05 00:08:49 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+// const number = 23;
+// const reminder = number % 10
+// const num = (number - reminder) / 10
+// console.log(num, " ", reminder)
+
+// // task 2
+// const number = 198;
+// const firstReminder = number % 100;
+// const firstDigit = (number - firstReminder) / 100 
+// const secondDigit = number % 10 
+// console.log(firstDigit, "", secondDigit)
+
+  //  task 3
+
+//  let student = 10;
+//  let apple = 100;
+//  console.log(apple/student)
+
+//task 4
+// let student = 10;
+// let apple = 100;
+// console.log(apple % student)
+
+// //task 5 
+// let stone  = 2;
+// let number = 1;
+// console.log(stone - number)
+
+// //task 6 
+// let a = 9;
+// let b = 4;
+// let perimeter = 2 * (a + b)
+// console.log(perimeter)
+
+//task 7 
+// let a = 12;
+// let b = 6;
+// const area = (a * b)
+// console.log(area)
+
+// // task 8
+// let a = 11;
+// let number =(a + 1)
+// console.log(number)
+
+// //task 9
+// let a = 17
+// let number = (a - 1)
+// console.log(number)
+
+// task 10
+// let number = 17;
+// let opposite = (number * -1)
+// console.log(opposite)
+
+// //task 11
+// let number = 14
+// let number3 = 16
+// number2 = (number + number3) / 2
+// console.log(number2)
+
+// //task 12
+// let a = 9
+// let b = 16
+// let c = 11
+// let plural = (a + b - c)
+// console.log(plural)
+
+
+
+
+
+
+
+
