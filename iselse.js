fb096f851ec69e2c78ea990a7974a023cecb556a
asmar <126621069+asmarkarimova@users.noreply.github.com>
2023-03-05 23:18:09 +0400

    Add files via upload

---
+++ iselse.js
@@ -0,0 +1,29 @@
+// //task 1
+// const number = 7;
+// if(number >=1) {
+//     console.log("Positive");
+// }
+// else if(number == 0){
+//     console.log(Neither, Positive, nor, Negative);
+// }
+// else{
+//     console.log("Negative")
+// }
+// //task 6
+// const n = 23;
+// if(n % 2 == 0) {
+// console.log("Even");
+// }
+// else{
+//     console.log("Odd")
+// }
+
+// //task 7
+// const number = 16;
+// const a = 7;
+// const b = 4;
+// const message = number % a == 0 ? "Yes" : "No";
+// console.log(message)
+
+//task 8
+
